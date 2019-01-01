88fc863e95dec2c6cf651bc8adfd0aefefd8f965
peterszabo <szabopeterakos@gmail.com>
2019-01-01 22:09:23 +0100

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Szabó Péter on 2019. 01. 01..
+//  Copyright © 2019. Szabó Péter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
